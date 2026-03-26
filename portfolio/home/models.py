4f1077896c200295f2b56d3011f6075ab613a326
ankushh1196-cloud <ankushh1196@gmail.com>
2026-03-26 23:43:01 +0530

    Delete portfolio directory

--- portfolio/home/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Contact(models.Model): #create a database table named contact
-    name = models.CharField(max_length=30)
-    email = models.EmailField()
-    desc=models.TextField()
-    def __str__(self):
-        return self.name +',' + self.email
-
